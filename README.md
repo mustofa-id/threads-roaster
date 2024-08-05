410698b0eba912a6d2fe31c0ce1012bf74fbf2c6
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2024-08-05 09:42:50 +0700

    chore: tidy up
    * docs
    
    * license
    
    * idiomatic load fn

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Roasting Threads User
+
+## Development
+
+1. Get a Gemini API key: [Gemini API Documentation](https://ai.google.dev/gemini-api/docs/api-key).
+2. Get your Supabase Project Ref and Anon key: [Supabase Dashboard](https://supabase.com/dashboard).
+3. Obtain the Chromium local path by running:
+
+   ```bash
+   pnpx @puppeteer/browsers install chromium@latest --path /your/true/path
+   ```
+
+4. Run the development server:
+
+   ```bash
+   pnpm dev
+   ```
+
+## Deployment
+
+Change the adapter based on your platform of choice. Read more in the [SvelteKit docs](https://kit.svelte.dev/docs/adapters).
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,42 +1,50 @@
 import { default_lang, roast_threads_user, supported_langs } from '$lib/server/api.js';
-import { isHttpError } from '@sveltejs/kit';
+import { isHttpError, redirect } from '@sveltejs/kit';
 
 export async function load({ url }) {
 	const username = url.searchParams.get('u');
-
-	let lang = /** @type {keyof supported_langs} */ (url.searchParams.get('l') || default_lang[0]);
-	let message = '';
-	let result = '';
+	const lang = /** @type {keyof supported_langs} */ (url.searchParams.get('l'));
 
 	if (!Object.keys(supported_langs).includes(lang)) {
-		lang = default_lang[0];
+		url.searchParams.set('l', default_lang[0]);
+		redirect(307, url);
 	}
 
-	if (username) {
-		if (is_username_valid(username)) {
-			try {
-				result = await roast_threads_user(username, lang);
-			} catch (error) {
-				console.error({ username, lang, error });
-				message = isHttpError(error)
-					? error.body?.message || 'Error tidak dikenal'
-					: 'Server error! Tidak dapat melakukan roasting sekarang';
-			}
-		} else {
-			message = 'Username tidak valid';
-		}
-	}
+	let result = '';
+	let message = '';
+
+	if (username) [result, message] = await start_roasting(username, lang);
 
 	return {
 		supported_langs,
 		setup: { username, lang },
-		message,
-		result
+		result,
+		message
 	};
 }
 
 /**
- * Based on this SO https://stackoverflow.com/a/17087528
+ *
+ * @param {string} username
+ * @param {keyof supported_langs} lang
+ * @returns {Promise<[string, string]>}
+ */
+async function start_roasting(username, lang) {
+	if (!is_username_valid(username)) return ['', 'Username tidak valid'];
+	try {
+		const result = await roast_threads_user(username, lang);
+		return [result, ''];
+	} catch (error) {
+		console.error({ username, lang, error });
+		const message = isHttpError(error)
+			? error.body?.message || 'Error tidak dikenal'
+			: 'Server error! Tidak dapat melakukan roasting sekarang';
+		return ['', message];
+	}
+}
+
+/**
+ * Based on https://stackoverflow.com/a/17087528
  *
  * @param {string} username
  * @returns {boolean}
